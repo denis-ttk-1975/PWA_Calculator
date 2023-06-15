c99f933176a564804af87a47806f6a6604ba10a1
denis-ttk-1975 <denis.ttk@gmail.com>
2023-06-15 23:32:12 +0300

    cash strategy with cashFirst added

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,22 @@
+const staticCacheName = 'static-den-calc-v1';
+
+const cashedAssets = ['index.html', 'main.js', 'style.css'];
+
 self.addEventListener('install', async (event) => {
   console.log('install');
+  const cache = await caches.open(staticCacheName);
+  await cache.addAll(cashedAssets);
 });
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  event.respondWith(cacheFirst(request));
+});
+
+async function cacheFirst(req) {
+  const cashed = await caches.match(req);
+  return cashed ?? fetch(req);
+}
 
 self.addEventListener('activate', async (event) => {
   console.log('activate');
