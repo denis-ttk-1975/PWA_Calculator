8c8e75428cc670c54a626dd57091723a3dec6d88
denis-ttk-1975 <denis.ttk@gmail.com>
2023-06-16 00:03:07 +0300

    fetch listener updated

--- service-worker.js
+++ service-worker.js
@@ -2,20 +2,34 @@ const staticCacheName = 'static-den-calc-v1';
 
 const cashedAssets = ['index.html', 'main.js', 'style.css'];
 
-self.addEventListener('install', async (event) => {
+self.addEventListener('install', (event) => {
   console.log('install');
-  const cache = await caches.open(staticCacheName);
-  await cache.addAll(cashedAssets);
+  event.waitUntil(async () => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(cashedAssets);
+  });
 });
 
-self.addEventListener('fetch', (event) => {
-  const { request } = event;
-  event.respondWith(cacheFirst(request));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(staticCacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
 });
 
 async function cacheFirst(req) {
   const cashed = await caches.match(req);
-  return cashed ?? fetch(req);
+  return cashed ?? (await fetch(req));
 }
 
 self.addEventListener('activate', async (event) => {
